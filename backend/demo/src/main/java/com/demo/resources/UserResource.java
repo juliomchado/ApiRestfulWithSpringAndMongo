ed62c1e4fa74c318a74604ad2772a6eab8cf7a19
Julio Machado <juliocarlos00@hotmail.com>
2020-12-15 08:08:29 -0300

    database auto instantiation

--- backend/demo/src/main/java/com/demo/resources/UserResource.java
+++ backend/demo/src/main/java/com/demo/resources/UserResource.java
@@ -1,11 +1,11 @@
 package com.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.demo.domain.User;
+import com.demo.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     // @RequestMapping(method = RequestMethod.GET)
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Silva", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+
+        List<User> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
     }
---
+++ backend/demo/src/main/java/com/demo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.demo.services;
+
+import java.util.List;
+
+import com.demo.domain.User;
+import com.demo.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
