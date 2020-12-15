e4e5b0fda637efc6b59f60274336c5263a743b87
Julio Machado <juliocarlos00@hotmail.com>
2020-12-15 09:06:56 -0300

    user delete

--- backend/demo/src/main/java/com/demo/resources/UserResource.java
+++ backend/demo/src/main/java/com/demo/resources/UserResource.java
@@ -10,11 +10,10 @@ import com.demo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,32 +26,30 @@ public class UserResource {
 
     // @RequestMapping(method = RequestMethod.GET)
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
-
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-
         return ResponseEntity.ok().body(listDto);
     }
 
-    @RequestMapping(value = "/{id}")
-    @GetMapping
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-
         User obj = service.findById(id);
-
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
-    
-    @PostMapping
+
+    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
         User obj = service.fromDTO(objDto);
-        
         obj = service.insert(obj);
-
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
