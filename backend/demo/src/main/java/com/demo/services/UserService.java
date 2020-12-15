91131e0131c99e863d8d625095bb7f04223e2241
Julio Machado <juliocarlos00@hotmail.com>
2020-12-15 08:54:38 -0300

    post user

--- backend/demo/src/main/java/com/demo/services/UserService.java
+++ backend/demo/src/main/java/com/demo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.demo.domain.User;
+import com.demo.dto.UserDTO;
 import com.demo.repository.UserRepository;
 import com.demo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not exists"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
