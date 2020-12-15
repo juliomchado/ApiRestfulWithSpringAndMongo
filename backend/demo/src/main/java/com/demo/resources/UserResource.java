679438da498e2feb35d6d8e2e78ced9b83f4936b
Julio Machado <juliocarlos00@hotmail.com>
2020-12-15 08:43:40 -0300

    find user by id

--- backend/demo/src/main/java/com/demo/resources/UserResource.java
+++ backend/demo/src/main/java/com/demo/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.demo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.demo.domain.User;
@@ -11,6 +10,7 @@ import com.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +30,14 @@ public class UserResource {
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}")
+    @GetMapping
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- backend/demo/src/main/java/com/demo/services/UserService.java
+++ backend/demo/src/main/java/com/demo/services/UserService.java
@@ -1,20 +1,26 @@
 package com.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.demo.domain.User;
 import com.demo.repository.UserRepository;
+import com.demo.services.exception.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository repo;
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not exists"));
     }
 }
